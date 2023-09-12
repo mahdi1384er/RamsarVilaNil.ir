b361f634ce378407d26ef03721d3bdf6e9ce58e8
mahdi bahrami <124788322+mahdi1384er@users.noreply.github.com>
2023-09-12 12:12:09 +0330

    finish projects RamsarBama

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./item.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'item.json'))
-const middlewares = jsonServer.defaults()
+server.use(middlewares);
+server.use(router);
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
-
-const PORT = 8000
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+server.listen(port);
