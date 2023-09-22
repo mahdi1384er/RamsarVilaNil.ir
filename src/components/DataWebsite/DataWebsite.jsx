8234920e77026feffceff66dc49c10ab2d8644eb
mahdi bahrami <124788322+mahdi1384er@users.noreply.github.com>
2023-09-22 21:16:25 +0330

    ad file jsonss

--- src/components/DataWebsite/DataWebsite.jsx
+++ src/components/DataWebsite/DataWebsite.jsx
@@ -1,12 +1,16 @@
+
+import img from '../../../public/img/imgs/8888888888888.jpg'
+import imgs from '../../../public/img/imgs/5555555.jpg'
+
 const DataWebsite = () => {
   return (
     <>
       <section className="w-full mt-[5px] mb-[10px] m-auto h-[60px] flex items-center justify-around">
         <div className="w-[46%] h-full">
-          <img src="../../../public/img/8888888888888.jpg" alt="" className="rounded-[5px]"/>
+          <img src={img} alt="" className="rounded-[5px]"/>
         </div>
         <div className="w-[46%]  h-full">
-          <img src="../../../public/img/5555555.jpg" alt="" className="rounded-[5px]"/>
+          <img src={imgs} alt="" className="rounded-[5px]"/>
         </div>
       </section>
     </>
