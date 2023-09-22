493211c4f821842b0591079b13db19f2b0e656a7
mahdi bahrami <124788322+mahdi1384er@users.noreply.github.com>
2023-09-22 10:35:16 +0330

    ad file jsons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import img from "../../../public/img/imgs/6.svg";
-import imgs from "../../../public/img/imgs/8.svg";
+import img from "../../../dist/img/imgs/6.svg";
+import imgs from "../../../dist/img/imgs/8.svg";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { React, useState, useEffect } from "react";
 import axios from "axios";
