306dbfc23be8d78a3e335d971e976580a639ba31
mahdi bahrami <124788322+mahdi1384er@users.noreply.github.com>
2023-09-22 11:00:38 +0330

    ad file jsons

--- src/components/navbars/NavBars.jsx
+++ src/components/navbars/NavBars.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import img from "../../../public/img/imgs/l.jpg";
+// import img from "../../../public/img/imgs/l.jpg";
 // import DrawerPlacement from "./Drawer";
 import { NavLink } from "react-router-dom";
 
@@ -47,7 +47,7 @@ const NavBarsScrool = () => {
             </span>
             <span>
             <NavLink to={"/"}>
-            <img src={img} alt="" className="w-[36px] rounded-[5px]" />
+            {/* <img src={img} alt="" className="w-[36px] rounded-[5px]" /> */}
             </NavLink>
             </span>
           </li>
