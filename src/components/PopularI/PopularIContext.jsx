cc64c1b408b5844148e9ef5525870b6ac4bd2be2
mahdi bahrami <124788322+mahdi1384er@users.noreply.github.com>
2023-09-27 11:18:13 +0330

    ad file jsonss

--- src/components/PopularI/PopularIContext.jsx
+++ src/components/PopularI/PopularIContext.jsx
@@ -3,7 +3,7 @@ import PopularItems from "./PopularItems";
 const PopularIContext = () => {
   return (
     <section className="w-full mt-[20px] m-auto">
-      <div className="w-[93%]   m-auto">
+      <div className="w-[95%]   m-auto mr-[9px] ">
         <div className="flex flex-col">
           <div>
             <h2 className="font-bold text-[18px] text-black h-[30px] relative left-[2px]">
@@ -11,7 +11,7 @@ const PopularIContext = () => {
             </h2>
           </div>
 
-          <div className="flex items-center justify-between overflow-scroll mt-[7px] ml-[-16px] mr-[-3px] ">
+          <div className="flex items-center justify-between overflow-scroll mt-[7px] ml-[-16px] mr-[-8px] ">
               <PopularItems />
           </div>
         </div>
